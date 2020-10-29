2f7c14a765f11cf1efcc0795a85c036e3c3929a9
Gowri Shankar J <gj@athenahealth.com>
2020-10-29 18:35:39 +0530

    first commit

---
+++ src/main/java/com/shankar/nisargika/NisargikaApplication.java
@@ -0,0 +1,13 @@
+package com.shankar.nisargika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisargikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisargikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shankar/nisargika/NisargikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankar.nisargika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisargikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
